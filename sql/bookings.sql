c76714ce09feee8452f15e357badb4faedcaa7c2
Jaymin Dattani <139145962+J-Dattani@users.noreply.github.com>
2024-05-02 18:44:35 +0530

    Add files via upload

---
+++ sql/bookings.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `your_table_name` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  `A_contact` DECIMAL(10, 0) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `tour` VARCHAR(255) NOT NULL,
+  `days` VARCHAR(255) NOT NULL,
+  `date` DATE NOT NULL,
+  `A_members` INT NOT NULL,
+  `C_members` INT NOT NULL,
+  `E_members` INT NOT NULL,
+  `Package` VARCHAR(255) NOT NULL
+);
